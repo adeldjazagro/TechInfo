48b3d249bed077fece63926b8cb67d905afe53a4
adeldjazagro <adeldjazagro@gmail.com>
2019-05-07 14:19:29 +0200

    first step

---
+++ TechInfo/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TechInfo
+//
+//  Created by Adel on 07/05/2019.
+//  Copyright © 2019 Adel. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TechInfo/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TechInfo
+//
+//  Created by Adel on 07/05/2019.
+//  Copyright © 2019 Adel. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
